c82b0acef3c4aa17b35de50d67d3721035d763a5
Isaac Shabtay <isaac@shabtay.com>
2019-11-25 04:30:31 -0800

    WIP

--- cloudify/scripts/ftp-upload.sh
+++ cloudify/scripts/ftp-upload.sh
@@ -1,2 +1,7 @@
 #!/bin/bash -e
-lftp -e "put ${resource_path}/index.html" -u ${username},${password} ${endpoint}
+app_dir=$(mktemp -d)
+cd ${app_dir}
+curl -o app.tar.gz ${app_content}
+mkdir extracted
+tar -C extracted -zxvf app.tar.gz
+lftp -e "mput extracted/*" -u ${username},${password} ${endpoint}
